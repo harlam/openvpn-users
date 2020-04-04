1eb6d2038d4f6aa4460e9b964ba29e38abac9628
harlam <harlamov.nikolay@yandex.ru>
2020-04-04 20:32:25 +0300

    add throws tag

--- src/Interfaces/StorageInterface.php
+++ src/Interfaces/StorageInterface.php
@@ -2,6 +2,8 @@
 
 namespace harlam\OpenVPN\Users\Interfaces;
 
+use harlam\OpenVPN\Users\Exceptions\UserNotFoundException;
+
 /**
  * Interface StorageInterface
  * @package harlam\OpenVPN\Users\Interfaces
@@ -11,6 +13,7 @@ interface StorageInterface
     /**
      * @param string $username
      * @return UserInterface
+     * @throws UserNotFoundException
      */
     public function findByUsername($username);
 }
